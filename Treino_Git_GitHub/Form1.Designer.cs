cc486024a98d01a058f23914524b47d0be5633c7
Augusto Wollace De Lima Rengel <augusto.rengel@siemens-healthineers.com>
2023-06-07 11:44:18 -0300

    criação de botões e funções para o treino

--- Treino_Git_GitHub/Form1.Designer.cs
+++ Treino_Git_GitHub/Form1.Designer.cs
@@ -28,84 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnAug = new System.Windows.Forms.Button();
+            this.btnCarlos = new System.Windows.Forms.Button();
+            this.btnGui = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblGithash = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnConflito1 = new System.Windows.Forms.Button();
+            this.btnConflito2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAug
             // 
-            this.button1.Location = new System.Drawing.Point(6, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 121);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAug.Location = new System.Drawing.Point(6, 25);
+            this.btnAug.Name = "btnAug";
+            this.btnAug.Size = new System.Drawing.Size(220, 121);
+            this.btnAug.TabIndex = 0;
+            this.btnAug.Text = "Augusto";
+            this.btnAug.UseVisualStyleBackColor = true;
+            this.btnAug.Click += new System.EventHandler(this.btnAug_Click);
             // 
-            // button2
+            // btnCarlos
             // 
-            this.button2.Location = new System.Drawing.Point(254, 25);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 121);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCarlos.Location = new System.Drawing.Point(254, 25);
+            this.btnCarlos.Name = "btnCarlos";
+            this.btnCarlos.Size = new System.Drawing.Size(220, 121);
+            this.btnCarlos.TabIndex = 1;
+            this.btnCarlos.Text = "Carlos";
+            this.btnCarlos.UseVisualStyleBackColor = true;
+            this.btnCarlos.Click += new System.EventHandler(this.btnCarlos_Click);
             // 
-            // button3
+            // btnGui
             // 
-            this.button3.Location = new System.Drawing.Point(503, 25);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 121);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnGui.Location = new System.Drawing.Point(503, 25);
+            this.btnGui.Name = "btnGui";
+            this.btnGui.Size = new System.Drawing.Size(220, 121);
+            this.btnGui.TabIndex = 2;
+            this.btnGui.Text = "Guilherme";
+            this.btnGui.UseVisualStyleBackColor = true;
+            this.btnGui.Click += new System.EventHandler(this.btnGui_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.btnAug);
+            this.groupBox1.Controls.Add(this.btnGui);
+            this.groupBox1.Controls.Add(this.btnCarlos);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(732, 161);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Trabalhando Individualmente";
+            this.groupBox1.Text = "Push sem conflitos";
             // 
-            // lblGithash
+            // groupBox2
             // 
-            this.lblGithash.AutoSize = true;
-            this.lblGithash.Location = new System.Drawing.Point(8, 498);
-            this.lblGithash.Name = "lblGithash";
-            this.lblGithash.Size = new System.Drawing.Size(148, 20);
-            this.lblGithash.TabIndex = 4;
-            this.lblGithash.Text = "Git Hash - 0000000";
+            this.groupBox2.Controls.Add(this.btnConflito1);
+            this.groupBox2.Controls.Add(this.btnConflito2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(732, 161);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Push com conflitos";
+            // 
+            // btnConflito1
+            // 
+            this.btnConflito1.Location = new System.Drawing.Point(115, 25);
+            this.btnConflito1.Name = "btnConflito1";
+            this.btnConflito1.Size = new System.Drawing.Size(220, 121);
+            this.btnConflito1.TabIndex = 0;
+            this.btnConflito1.Text = "Conflito 1";
+            this.btnConflito1.UseVisualStyleBackColor = true;
+            this.btnConflito1.Click += new System.EventHandler(this.btnConflito1_Click);
+            // 
+            // btnConflito2
+            // 
+            this.btnConflito2.Location = new System.Drawing.Point(396, 25);
+            this.btnConflito2.Name = "btnConflito2";
+            this.btnConflito2.Size = new System.Drawing.Size(220, 121);
+            this.btnConflito2.TabIndex = 1;
+            this.btnConflito2.Text = "Conflito 2";
+            this.btnConflito2.UseVisualStyleBackColor = true;
+            this.btnConflito2.Click += new System.EventHandler(this.btnConflito2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(758, 527);
-            this.Controls.Add(this.lblGithash);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnAug;
+        private System.Windows.Forms.Button btnCarlos;
+        private System.Windows.Forms.Button btnGui;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label lblGithash;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConflito1;
+        private System.Windows.Forms.Button btnConflito2;
     }
 }
 
--- Treino_Git_GitHub/Form1.cs
+++ Treino_Git_GitHub/Form1.cs
@@ -16,5 +16,49 @@ namespace Treino_Git_GitHub
         {
             InitializeComponent();
         }
+
+        private void btnAug_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCarlos_Click(object sender, EventArgs e)
+        {
+            // TODO: gerar popup com os 10 primeiros numeros da sequencia de fibonacci separados por ;
+            int i = 0;
+            int lastNumber = 0;
+            int currentNumber = 1;
+            while (i < 10)
+            {
+                MessageBox.Show(currentNumber.ToString());
+                currentNumber += lastNumber;
+                lastNumber *= currentNumber;
+            }
+
+        }
+
+        private void btnGui_Click(object sender, EventArgs e)
+        {
+            // TODO: gerar popup com os 10 primeiros numeros da sequencia de fibonacci separados por ;
+            int i = 0;
+            int lastNumber = 0;
+            int currentNumber = 1;
+            while (i < 10)
+            {
+                MessageBox.Show(currentNumber.ToString());
+                currentNumber += lastNumber;
+                lastNumber *= currentNumber;
+            }
+        }
+
+        private void btnConflito1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConflito2_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
